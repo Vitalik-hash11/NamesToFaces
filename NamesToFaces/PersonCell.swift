4c997f75ba7bb619a13d2b7c9ccd20774b8f3084
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-08-31 17:14:18 +0300

    prepare ui

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by newbie on 30.08.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var personLabel: UILabel!
+    
+}
